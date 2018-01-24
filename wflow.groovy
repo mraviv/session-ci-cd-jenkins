7d93b7cff6b6be7f16694d16a6dd9f71008e8f86
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-01-24 13:20:22 +0200

    removed parallel

--- wflow.groovy
+++ wflow.groovy
@@ -1,5 +1,4 @@
-parallel (
-        node ('opsschool-slaves'){
+node ('opsschool-slaves'){
     currentBuild.result = "SUCCESS"
 
     stage('Checkout'){
@@ -30,37 +29,4 @@ parallel (
 
     junit 'test/reports/*.xml'
 
-},
-        node ('ops-school-dynamic-slave'){
-            currentBuild.result = "SUCCESS"
-
-            stage('Checkout'){
-
-                checkout scm
-            }
-
-            stage('Test'){
-                sh 'ruby app/tc_ruby_app.rb'
-
-                sh 'ls -ltrh test/reports/'
-            }
-
-            stage('Build'){
-                sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
-                sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
-                sh 'sudo docker-compose stop'
-
-                sh 'sudo docker rm -f opsschool_dummy_app || true'
-                sh 'sudo docker rm -f dummyappbuildworkflow_nginx_1 || true'
-            }
-
-            stage('Deploy'){
-                sh 'sudo docker pull localhost:5000/opsschool_dummy_app:latest'
-                sh 'sudo docker-compose up -d'
-            }
-
-            junit 'test/reports/*.xml'
-
-        }
-
-)
+}
